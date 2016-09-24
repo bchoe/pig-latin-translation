dcabc65c9f491ba9e1284f1c31101c7c2feeab57
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-23 19:22:57 -1000

    commit

--- test/salutation.spec.js
+++ test/salutation.spec.js
@@ -1,3 +1,22 @@
 const Chai = require('chai');
+const Expect = Chai.expect;
+const pigLatin = require('../pig-latin');
+const myPig = pigLatin();
 
-console.log(Chai);
+describe ('PigLatin', () => {
+
+it('should be an object', () => {
+  Expect(myPig).to.be.an('object');
+});
+
+it('should return a pig word', () =>{
+  Expect(myPig.pigWord('eat')).to.equal('eatay');
+  Expect(myPig.pigWord('omelet')).to.equal('omeletay');
+  Expect(myPig.pigWord('are')).to.equal('areay');
+  Expect(myPig.pigWord('pig')).to.equal('ig-pay');
+  Expect(myPig.pigWord('banana')).to.equal('anana-bay');
+  Expect(myPig.pigWord('trash')).to.equal('ash-tray');
+
+});
+
+});
